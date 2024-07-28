71710819358340e829df216e50ffcb34705d5bf0
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-07-28 07:55:00 +0700

    update

--- api/hello.go
+++ api/hello.go
@@ -3,8 +3,23 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World from Go!")
+}
+
+func HelloFromID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprintf(w, "Hello with ID: %s", id)
+}
+
+func SetupRoutes() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HelloWorld).Methods("GET")
+	r.HandleFunc("/{id}", HelloFromID).Methods("GET")
+	return r
 }
--- api/main.go
+++
@@ -1,7 +0,0 @@
-package handler
-
-import "net/http"
-
-func main() {
-	http.HandleFunc("/api/hello", HelloWorld)
-}
